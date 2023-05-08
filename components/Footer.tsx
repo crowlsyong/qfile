3b97fcf9d3741ebb870cb99e92635d043ae7d58a
Crypto Cathedral <81304661+crowlsyong@users.noreply.github.com>
2023-05-08 17:12:52 -0500

    updated logos n stuff
    graphics

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import UserIP from '../islands/UserIP.tsx';
 export default function Footer() {
   return (
   <footer
-    class={tw`bottom-0 w-full h-14 lg:px-36 md:px-20 sm:px-16 px-10 bg-gray-900 absolute flex items-center justify-center text-white font-base sm:text-base text-sm`}
+    class={tw`bottom-0 w-full h-14 lg:px-36 md:px-20 sm:px-16 px-10 bg-black absolute flex items-center justify-center text-white font-base sm:text-base text-sm`}
   >
     <div class={tw`mr-2 flex items-center`}>
       <span class={tw`mr-1`}>pwning:</span>
